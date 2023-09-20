9d448e1cd3e123611fc80b14091eb74bada79580
Bartek Karczmarczyk <bartlomiej.karczmarczyk@igt.com>
2023-09-20 13:18:16 +0200

    Added some info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,20 @@
 <div id="content">
   <h1>Hexdump Helper</h1>
+  <ol>
+    <li>Paste hexdump into input field</li>
+    <li>Strip the byte count markers by clicking the button below</li>
+    <li>Select bytes to decode</li>
+  </ol>
   <textarea id="input_field" name="input_field" placeholder="Paste hexdump here" bind:value on:select={updateResults} rows="25" cols="125" />
   <p/>
   <button on:click={strip}>Strip byte markers</button>
   <p>Selected bytes: {hex.split(" ").length}</p>
   <hr>
-  <p>Hex:</p>
-  <p>{hex}</p>
+  <p><b>Numbers</b></p>
+  <p>Hex: {hex}</p>
+  <p>Int: {intResult}</p>
   <hr>
-  <p>Int:</p>
-  <p>{intResult}</p>
-  <hr>
-  <p>Str:</p>
+  <p><b>String</b></p>
   <p>{strResult}</p>
 </div>
 <script>
